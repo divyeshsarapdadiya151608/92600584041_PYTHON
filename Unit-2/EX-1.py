e9cf5640e6288da8d958a2a50d7446128f07390a
92600584041-divyesh-sarapdadiya <divyesh.sarapdadiya151608@marwadiuniversity.ac.in>
2026-09-14 14:45:41 +0530

    Update EX-1.py

--- Unit-2/EX-1.py
+++ Unit-2/EX-1.py
@@ -1,1 +1,28 @@
+# Program to demonstrate conditional statements
+# Using if statement
+age = int(input("Enter your age: "))
 
+if age >= 18:
+    print("You are eligible to vote.")
+
+
+# Using if-else statement
+number = int(input("\nEnter a number: "))
+
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
+
+# Using if-elif-else statement
+marks = int(input("\nEnter your marks: "))
+
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 75:
+    print("Grade: B")
+elif marks >= 50:
+    print("Grade: C")
+else:
+    print("Grade: F")
