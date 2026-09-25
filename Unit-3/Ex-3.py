756529715e1e974b5361dfde12891a6dd8b1dd34
92600584041-divyesh-sarapdadiya <divyesh.sarapdadiya151608@marwadiuniversity.ac.in>
2026-09-25 10:23:44 +0530

    Add mathematical operations using math module
    This program demonstrates various mathematical operations using the math module, including square root, power, factorial, ceiling, floor, and the value of PI.

---
+++ Unit-3/Ex-3.py
@@ -0,0 +1,12 @@
+#Write a program to perform mathematical operations using math module.
+import math
+
+num = 25
+
+print("Number:", num)
+print("Square root:", math.sqrt(num))
+print("Power:", math.pow(2, 3))
+print("Factorial:", math.factorial(5))
+print("Ceiling:", math.ceil(4.3))
+print("Floor:", math.floor(4.8))
+print("Value of PI:", math.pi)
